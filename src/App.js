ef934043ec7fb1b3a346006d082a2830e2739397
Ilya Okunev <zhoramc1@gmail.com>
2022-01-23 14:07:39 +0300

    feat: rewrite input update algorytm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useState, useEffect } from 'react/cjs/react.development';
+import CurrencyRow from './CurrencyRow';
+
+
+function Header() {
+  return (
+    <div className="header-container">
+      <h2 className="header-title">Welcome to Crypto Converter</h2>
+    </div>
+  )
+};
+
+function Body() {
+
+  let [exchangeRates, setExRates ] = useState([
+    {id: 0, rate: 1, symbol: 'USD', amount: ''},
+    {id: 1, rate: 1, symbol: 'USD', amount: ''}
+  ]);
+
+  let [currArr, setArr] = useState([]);
+  let [lastChangedInput,setLastChangeInput] = useState();
+
+  useEffect(() => {
+    let cleanup = false;
+    (async function Request() {
+      let response = await fetch('https://crypto-kek.herokuapp.com/', {
+        headers: {
+          Accept: 'application/json',
+        }
+      });
+      let result = await response.json();
+      setArr(result);
+    })();
+    return () => cleanup = true;
+  },[]);
+
+  function tokenChange(id,value) {
+    setExRates( exchangeRates => {
+      let newRates = Object.assign({}, exchangeRates);
+      newRates[id].symbol = value;
+      if(value === 'USD') {
+        newRates[id].rate = 1;
+      } else {
+        let actualToken = currArr.filter(token => token.symbol === value)[0];
+        newRates[id].rate =  actualToken.quote.USD.price;
+      }
+      amountChange(lastChangedInput, exchangeRates[lastChangedInput].amount)
+      return newRates;
+    });
+  }
+
+  function amountChange(id, value) {
+    let currency = exchangeRates[id].rate / exchangeRates[Math.abs(id - 1)].rate
+    setExRates( 
+      (exchangeRates) => {
+      let newRates = Object.assign({}, exchangeRates);
+        newRates[id].amount = value;
+        newRates[Math.abs(id - 1)].amount = value ? currency * +value : '';
+      return newRates;
+    }
+    );
+    setLastChangeInput(id);
+  }
+
+  return (
+    <div className='converter-body'>
+      <CurrencyRow exchangeRates ={exchangeRates[0]} currArr={currArr} tokenChange={tokenChange} inputChange={amountChange} />
+      <br/>
+      <CurrencyRow exchangeRates={exchangeRates[1]} currArr={currArr} tokenChange={tokenChange} inputChange={amountChange} />
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      <Body/>
     </div>
   );
 }
---
+++ src/CurrencyRow.js
@@ -0,0 +1,15 @@
+import { cleanup } from '@testing-library/react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+export default function CurrencyRow({currArr, exchangeRates, tokenChange, inputChange}) {
+  return (
+    <div className="input-container">
+      <input className="input" type="number" onChange={(e) => inputChange(exchangeRates.id, e.target.value)} value={exchangeRates.amount}/>
+      <select className="select" onChange={(e) => tokenChange(exchangeRates.id, e.target.value)}>
+        <option value='USD'>USD</option>
+        {(cleanup) ? currArr.map((token, i) => <option key={i+1} value={token.symbol}>{token.symbol}</option>) : null}
+      </select>
+    </div>
+  )
+}
